607d6a2737c0547c8c37765a30e24347180ebf2f
Umang Madeshia <umang.madeshia1795@gmail.com>
2023-04-13 09:57:48 +0530

    Added solution - LeetHub

--- Print N to 1 without loop - GFG/print-n-to-1-without-loop.java
+++ Print N to 1 without loop - GFG/print-n-to-1-without-loop.java
@@ -23,18 +23,15 @@ class Solution {
 
     void printNos(int N) {
         // code here
-    
-          helper(N);
-      
-    }
-    public void helper(int N){
-         if(N!=1){
-         System.out.print(N+" "); }
-         if(N==1){
+        
+        if(N==1){
             System.out.print(1);
             return;
-        }    
+        }
+        System.out.print(N+" ");
         printNos(N-1);
+        
       
     }
+   
 }
